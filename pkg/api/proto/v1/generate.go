9311bda3d34d4b9a89639810cd1787b0160d1a89
Diana Bejan <diana.bejan@brainloop.com>
2019-03-24 21:41:15 +0200

    Implement repository

--- pkg/api/proto/v1/generate.go
+++ pkg/api/proto/v1/generate.go
@@ -1,3 +1,4 @@
 package v1
 
-//go:generate protoc -I/usr/local/include -I. -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:./../../../server/pb service.proto
+// go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+//go:generate protoc -I/usr/local/include -I. -I$GOPATH/src --go_out=plugins=grpc:./../../../../pb service.proto
